b06b157f3633e9f7abe65d142c9973796e87aa99
Amit4218 <amitbhagat621@gmail.com>
2025-12-12 16:51:49 +0530

    added routes for about & contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/*" element={<Error />} />
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
